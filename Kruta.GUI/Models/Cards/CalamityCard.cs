6e3963a9378ba3858dad9275cc6dda2a5c20e082
EgorSalahov <egorsalahov2@gmail.com>
2025-12-09 14:28:31 +0300

    Добавьте файлы проекта.

---
+++ Kruta.GUI/Models/Cards/CalamityCard.cs
@@ -0,0 +1,18 @@
+﻿using Kruta.Shared.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kruta.Shared.Models.Cards
+{
+    // Беспредел (специальный класс из-за особой механики)
+    public class CalamityCard : Card
+    {
+        public CalamityCard()
+        {
+            Type = CardType.Calamity;
+        }
+
+        // Дополнительные свойства, если нужны (например, цель атаки)
+    }
+}
